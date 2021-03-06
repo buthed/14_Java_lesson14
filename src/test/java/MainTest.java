8acfd0f2dbfd7b6228e93c245d561023f11abd23
buthed <temaTihonov@yandex.ru>
2021-03-06 18:28:48 +0300

    Last version

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,10 +2,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -13,9 +12,22 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 class MainTest {
 
 
-    @Test
-    void task1Test() {
+
+    static Stream<Arguments> arrayPlusArray() {
+        return Stream.of(
+                Arguments.of((Object) new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7 }, (Object) new int[]{1, 7}),
+                Arguments.of((Object) new int[]{1, 4, 4, 1, 1, 4, 3}, (Object) new int[]{3}),
+                Arguments.of((Object) new int[]{1, 2, 4, 4, 2, 3, 1, 7 }, (Object) new int[]{2, 3, 1, 7}),
+                Arguments.of((Object) new int[]{1, 2, 4, 4, 4, 3, 1, 7 }, (Object) new int[]{3, 1, 7})
+        );
     }
+    @ParameterizedTest
+    @MethodSource("arrayPlusArray")
+    void task1Test(int[] arr1, int[] arr2) {
+        Assertions.assertEquals(Arrays.toString(arr2), Arrays.toString(Main.task1(arr1)));
+    }
+
+
 
 
 
@@ -27,10 +39,9 @@ class MainTest {
                 Arguments.of((Object) new int[]{1, 4, 4, 1, 1, 4, 3}, false)
         );
     }
-
     @ParameterizedTest
     @MethodSource("arrayPlusBul")
-     void task2Test(int[] arr, boolean b) {
+    void task2Test(int[] arr, boolean b) {
         Assertions.assertEquals(b, Main.task2(arr));
     }
 
